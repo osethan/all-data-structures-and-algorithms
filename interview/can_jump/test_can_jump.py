5e31a74d8cafeb642c08e17b76cb4d0b53651a2c
osethan <GE47R9xmtouch .zshrcFASEPs@0>
2020-06-03 13:17:56 -0700

    Problem written

---
+++ interview/can_jump/test_can_jump.py
@@ -0,0 +1,10 @@
+import pytest
+from intervew.can_jump.can_jump import can_jump
+
+@pytest.mark.parametrize('lst, expected', [
+  ([2,3,1,1,4], True),
+  ([3,2,1,0,4], False)
+])
+def test_can_jump(lst, expected):
+  actual = can_jump(lst)
+  assert actual == expected
